9e0a83872461021d44f0d8b1176f94f93b5261a2
Nyko77771 <nykytamcdonald@gmail.com>
2024-08-04 00:28:04 +0100

    main change

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const path = require("path");
 const app = express();
 app.set("view engine", "ejs");
 
+//Add Database Methods
+const mongoDB = require("./database/mongoDB");
+
 const port = 8080;
 const localhost = "127.0.0.1";
 
-app.use(express.static(path.join(__dirname, "views")));
+app.use(express.static(path.join(__dirname, "public")));
 
+//Local
 app.listen(port, localhost, (error) => {
   if (error) {
     console.log(`An error ${error} has occured`);
@@ -25,10 +29,22 @@ app.get("/", (req, res) => {
   }
 });
 
+//Depreciated Search Function
+/*
 app.get("/search", (req, res) => {
   try {
     console.log("Established connection");
     res.render("search");
+  } catch (error) {
+    console.log(`An ${error} has occured`);
+  }
+});
+*/
+
+app.get("/prescriptions", (req, res) => {
+  try {
+    console.log("Established connection");
+    res.render("prescriptions");
   } catch (error) {
     console.log(`An ${error} has occured`);
   }
